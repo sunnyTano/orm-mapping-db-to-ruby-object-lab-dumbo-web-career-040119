ac65bfbb219914f2c4f324bffa9fb9551eba2b85
Susan Tan <ssusantan9@gmail.com>
2019-04-11 02:33:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
     WHERE grade = ?
       SQL
     
-      DB[:conn].execute(sql, grade).map do |row|
+      DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end
   end 
